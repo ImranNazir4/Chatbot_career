fc080ed2e9e0476dd8d109098dead453354d636f
Imran Nazir <106988509+ImranNazir4@users.noreply.github.com>
2025-01-10 21:49:36 +0500

    Update chat_with_data.py

--- chat_with_data.py
+++ chat_with_data.py
@@ -23,11 +23,11 @@ load_dotenv()
 GROQ_API_KEY=os.getenv("GROQ_API_KEY")
 llm = ChatGroq(
     temperature=0,
-    model="llama3-70b-8192",
+    model="mixtral-8x7b-32768",
     api_key=GROQ_API_KEY
 )
 ###################################################################################################################################
-loader = PyPDFLoader("Artificial Intelligence for Career Guidance – Current Requirements and Prospects for the Future.pdf")
+loader = PyPDFLoader("EUAS_Tuition and service fees_2024-2025-2.pdf")
 pages = loader.load()
 chunk_size =500
 chunk_overlap = 100
